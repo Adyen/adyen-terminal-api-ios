4d7a57d49744112c5e0c4a3c6662c06013958607
Yurii <yurii.zadoianchuk@gmail.com>
2022-10-24 13:42:05 +0200

    Revert "Add tests for EncryptedMessage"
    This reverts commit d716adb12b8d700276a0446d6efef4b1b12aa46a.

---
+++ Tests/TerminalAPIKitTests/TerminalAPIKitTests.swift
@@ -0,0 +1,13 @@
+//
+//  TerminalAPIKitTests.swift
+//  TerminalAPIKit
+//
+//  Copyright (c) 2022 Adyen N.V.
+//
+
+@testable import TerminalAPIKit
+import XCTest
+
+final class TerminalAPIKit: XCTestCase {
+    func testExample() {}
+}
