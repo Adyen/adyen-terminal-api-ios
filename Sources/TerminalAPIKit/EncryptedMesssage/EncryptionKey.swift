63e5ba89007a489c87c1a8e52c11c2130ab0d6fe
yurii <yurii.zadoianchuk@adyen.com>
2022-11-01 10:01:11 +0100

    Add EncryptionKey derivation helpers

--- Sources/TerminalAPIKit/EncryptedMesssage/EncryptionKey.swift
+++ Sources/TerminalAPIKit/EncryptedMesssage/EncryptionKey.swift
@@ -6,6 +6,15 @@
 //
 
 import Foundation
+import CommonCrypto
+
+/// Describes errors that can happen during der
+public struct KeyDerivationError: Error {
+    
+    /// The result code of the failed key derivation.
+    public let errorCode: CCStatus
+    
+}
 
 /// A key used to encrypt and decrypt messages.
 public struct EncryptionKey {
@@ -31,6 +40,34 @@ public struct EncryptionKey {
         self.identifier = identifier
         self.version = version
         self.data = data
+    }
+    
+    // MARK: - Key derivation
+    
+    /// Derives a key from given passhprage, identifier and version.
+    ///
+    /// The `passphrase`, `identifier` and `version` can be found in the terminal settings in the Customer Area,
+    /// under Integrations -> Terminal API -> Encryption key. Pass them in a string form, exactly as they appear.
+    ///
+    /// - Parameters:
+    ///   - passphrase: The passphrase of the key.
+    ///   - identifier: The identifier of the key.
+    ///   - version: The version of the key.
+    ///
+    /// - Throws: ``KeyDerivationError`` if key derivation was unsuccessful.
+    public init(
+        passphrase: String,
+        identifier: String,
+        version: UInt
+    ) throws {
+        let keyData = try Self.pbkdf2SHA1(
+            passphrase: passphrase,
+            salt: Data(Self.saltphrase.utf8),
+            keyByteCount: Self.keyLength,
+            rounds: Self.keyDerivationPasses
+        )
+        
+        self.init(identifier: identifier, version: version, data: keyData)
     }
     
     // MARK: - Key Slices
@@ -74,6 +111,39 @@ public struct EncryptionKey {
     
     private var initializationVectorRange: Range<Int> {
         return aesKeyRange.upperBound..<aesKeyRange.upperBound + EncryptionKey.initializationVectorLength
+    }
+    
+    private static let saltphrase = "AdyenNexoV1Salt"
+    private static let keyLength = 80
+    private static let keyDerivationPasses = 4000
+    
+    private static func pbkdf2SHA1(
+        passphrase: String,
+        salt: Data,
+        keyByteCount: Int,
+        rounds: Int
+    ) throws -> Data {
+        
+        let passphraseData = Data(passphrase.utf8)
+        var derivedKeyData: [UInt8] = .init(repeating: 0, count: keyByteCount)
+        
+        let derivationStatus = CCKeyDerivationPBKDF(
+            CCPBKDFAlgorithm(kCCPBKDF2),
+            passphrase,
+            passphraseData.count,
+            salt.bytes,
+            salt.count,
+            CCPBKDFAlgorithm(kCCPRFHmacAlgSHA1),
+            UInt32(rounds),
+            &derivedKeyData,
+            derivedKeyData.count
+        )
+        
+        guard derivationStatus == 0 else {
+            throw KeyDerivationError(errorCode: derivationStatus)
+        }
+        
+        return Data(derivedKeyData)
     }
     
 }
