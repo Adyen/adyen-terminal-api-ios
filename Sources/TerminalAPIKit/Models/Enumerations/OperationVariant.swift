26d0d3fbdfecdefe76a1788a9d187b7b183e93cc
amcalgates <agates@guestinnovations.com>
2023-09-05 09:14:47 -0700

    Per Yurii@Adyen: - Add `OperationVariant` enum - Add comment stubs for inits to match rest of code base - Fix spacing in variable declarations - Capitalize first word in comments

---
+++ Sources/TerminalAPIKit/Models/Enumerations/OperationVariant.swift
@@ -0,0 +1,12 @@
+//
+//  OperationVariant.swift
+//  
+//
+//  Created by Andrew on 9/5/23.
+//
+
+import Foundation
+
+public enum OperationVariant: String, Codable {
+    case waitingScreen = "WaitingScreen"
+}
--- Sources/TerminalAPIKit/Models/Operation.swift
+++ Sources/TerminalAPIKit/Models/Operation.swift
@@ -9,16 +9,21 @@ import Foundation
 
 public final class Operation: Codable {
 
-    /// the type of request after which the waiting screen shows: InputRequest, DisplayRequest, and Payment
-    public let type:OperationType
+    /// The type of request after which the waiting screen shows: InputRequest, DisplayRequest, and Payment
+    public let type: OperationType
     
     /// For now, the Operation.Variant can only have WaitingScreen as value. This shows the waiting screen animation on the terminal after every request until you end the session.
-    public let variant:String
+    public let variant: OperationVariant
     
     /// UserInterfaceData.WaitingScreenTimeoutMs: how long the waiting screen is shown on the terminal display, in milliseconds.
-    public let userInterfaceData:UserInterfaceData
+    public let userInterfaceData: UserInterfaceData
     
-    public init(type: OperationType, variant: String, userInterfaceData: UserInterfaceData) {
+    /// Initializes the Operation.
+    ///
+    /// - Parameter type: Undocumented.
+    /// - Parameter variant: Undocumented.
+    /// - Parameter userInterfaceData: Undocumented.
+    public init(type: OperationType, variant: OperationVariant, userInterfaceData: UserInterfaceData) {
         self.type = type
         self.variant = variant
         self.userInterfaceData = userInterfaceData
